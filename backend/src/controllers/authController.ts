9729effa0bfe2cb103c2c305d85ad624be96575e
Danil Zabelin <danil.danich.22825@gmail.com>
2026-05-13 17:46:25 +0300

    Добавлены новые контроллеры на бэк

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,9 @@
 import {Request, Response} from "express";
 import {prisma} from "../prismaClient.ts";
 import bcrypt from "bcrypt";
+import {createAccessToken, createRefreshToken, verifyToken} from "@/utils/jwt.ts";
+import {User} from "../../prisma/generated/client.ts";
+import jwt from "jsonwebtoken";
 
 const register = async (req: Request, res: Response) => {
     try {
@@ -24,46 +27,26 @@ const register = async (req: Request, res: Response) => {
         const newUser = await prisma.user.create({
             data: {
                 email,
-                username: "user",
-                role: "user",
-                password: passwordHash,
-                deletedAt: null,
+                fullName: "user",
+                hashedPassword: passwordHash,
             },
             select: {
                 id: true,
             },
         });
-
-        // const accessToken = createAccessToken({userId: newUser.id});
-        // const refreshToken = createRefreshToken({userId: newUser.id});
 
         const updatedUser = await prisma.user.update({
             where: {id: newUser.id},
             data: {
-                username: `user-${newUser.id}`,
-                // refresh_token: refreshToken,
+                fullName: `user-${newUser.id}`,
             },
             select: {
                 id: true,
                 email: true,
-                username: true,
-                role: true,
+                fullName: true,
                 createdAt: true,
             },
         });
-        // res.cookie("accessToken", accessToken, {
-        //     httpOnly: true,
-        //     sameSite: "lax",
-        //     secure: false,
-        //     maxAge: 15 * 60 * 1000
-        // });
-        // res.cookie("refreshToken", refreshToken, {
-        //     httpOnly: true,
-        //     sameSite: "lax",
-        //     secure: false,
-        //     path: "/api/auth/refresh-token",
-        //     maxAge: 7 * 24 * 60 * 60 * 1000
-        // });
 
         res.status(201).json({message: "User successfully created", user: updatedUser, error: null});
     } catch (error) {
@@ -84,3 +67,157 @@ const register = async (req: Request, res: Response) => {
         res.status(500).json({message: "Internal server error", error: (error as Error).message});
     }
 }
+
+const login = async (req: Request, res: Response) => {
+    try {
+        const {email, password} = req.body as { email?: string; password?: string };
+
+        if (!email || !password) {
+            res.status(400).json({message: "Почта и пароль обязательны."})
+            return;
+        }
+
+        const userPassword = await prisma.user.findFirst({
+            where: {email},
+            select: {
+                hashedPassword: true,
+            }
+        });
+
+        if (!userPassword) {
+            res.status(401).json({message: "Неверная почта или пароль."})
+            return;
+        }
+
+        const isValidPassword = await bcrypt.compare(password, userPassword.hashedPassword)
+        if (!isValidPassword) {
+            res.status(401).json({message: "Неверная почта или пароль."})
+            return;
+        }
+
+        const user = await prisma.user.findFirst({
+            where: {email},
+            select: {
+                id: true,
+                email: true,
+                fullName: true,
+                createdAt: true,
+            }
+        })
+
+        if (!user) {
+            console.error("Ошибка при поиске пользователя")
+            return;
+        }
+
+        const accessToken = createAccessToken({userId: user.id})
+        let refreshToken = req.cookies.refreshToken || "";
+
+        if (refreshToken) {
+            const decoded = verifyToken(refreshToken);
+
+            const session = await prisma.sessions.findUnique({
+                where: {id: decoded.sessionId},
+
+                select: {
+                    isActive: true,
+                },
+            });
+
+            if (!session.isActive) {
+                res.status(403).json({message: "Forbidden."})
+                return;
+            } else {
+                res.cookie("accessToken", decoded.accessToken, {
+                    httpOnly: true,
+                    sameSite: "lax",
+                    secure: false,
+                    maxAge: 15 * 60 * 1000
+                })
+                res.json({message: "Логин успешен.", user, error: null});
+                return;
+            }
+        }
+
+        const session = await prisma.sessions.create({
+            data: {isActive: true},
+            select: {
+                id: true,
+            }
+        })
+
+        refreshToken = createRefreshToken({userId: user.id, sessionId: session.id})
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            sameSite: "lax",
+            secure: false,
+            maxAge: 15 * 60 * 1000
+        });
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            sameSite: "lax",
+            secure: false,
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        res.json({message: "Логин успешен.", user, error: null})
+    } catch (error) {
+        console.error("Error in login:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+const logout = async (req: Request, res: Response) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        const decoded = verifyToken(refreshToken);
+
+        await prisma.sessions.update({
+            where: {id: decoded.sessionId},
+            data: {isActive: false},
+        })
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+
+        res.status(204).send()
+    } catch (error) {
+        console.error("Error in logout:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+const refreshToken = async (req: Request, res: Response) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) {
+            res.status(401).json({message: "Не авторизован"});
+            return;
+        }
+        const decoded = verifyToken(refreshToken);
+        const user = await prisma.user.findFirst({
+            where: {id: decoded.userId},
+        });
+        if (!user) {
+            res.status(401).json({message: "Пользователя не существует"})
+            return;
+        }
+
+        const accessToken = createAccessToken({userId: user.id});
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            sameSite: "lax",
+            secure: false,
+            maxAge: 15 * 60 * 1000
+        })
+        res.status(200).json({message: "Token refreshed"})
+    } catch (error) {
+        console.error("Error in refreshToken:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+// const getMe = async (req: Request, res: Response) => {
+//     res.json({userId: req.userId})
+// }
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -5,11 +5,11 @@ const jwtSecret = env.JWT_SECRET;
 const accessExpiresIn = env.JWT_ACCESS_EXPIRES_IN;
 const refreshExpiresIn = env.JWT_REFRESH_EXPIRES_IN;
 
-export const createAccessToken = (payload: { userId: number }): string => {
+export const createAccessToken = (payload: { userId: string }): string => {
     return jwt.sign(payload, jwtSecret, {expiresIn: accessExpiresIn});
 };
 
-export const createRefreshToken = (payload: { userId: number, sessionId: number }): string => {
+export const createRefreshToken = (payload: { userId: string, sessionId: string }): string => {
     return jwt.sign(payload, jwtSecret, {expiresIn: refreshExpiresIn});
 };
 
